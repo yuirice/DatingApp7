63472d8a5a9e775f30858e25323c6b7a3e9f5149
Yuirice <yulirice@gmail.com>
2023-08-22 11:49:55 +0800

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -11,11 +11,12 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt =>{
  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseHttpsRedirection();
 
