fdeff0b055428c0380aca0e51267933a526c2f8b
Yuirice <yulirice@gmail.com>
2023-09-12 22:54:04 +0800

    End of section 9-1

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -18,9 +18,24 @@ import { TestErrorComponent } from './errors/test-error/test-error.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 
 @NgModule({
-    declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent, MemberListComponent, MemberDetailComponent, ListsComponent, MessagesComponent, TestErrorComponent, NotFoundComponent,  ServerErrorComponent],
+    declarations: [
+      AppComponent,
+      NavComponent,
+      HomeComponent,
+      RegisterComponent,
+      MemberListComponent,
+      MemberDetailComponent,
+      ListsComponent,
+      MessagesComponent,
+      TestErrorComponent,
+      NotFoundComponent,
+      ServerErrorComponent,
+      MemberCardComponent
+    ],
     imports: [
       BrowserModule,
       AppRoutingModule,
@@ -30,7 +45,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
       SharedModule
     ],
     providers: [
-       {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+       {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+       {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
     ],
     bootstrap: [AppComponent],
 })
